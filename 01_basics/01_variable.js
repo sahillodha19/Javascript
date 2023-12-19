b5c4614060e91b5ddd8d8d45e8649be668405923
sahillodha19 <sahillodha170@yahoo.com>
2023-12-19 08:20:20 +0000

    Datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 144532;
+let accountEmail = "Sahillodha1903@gmail.com";
+var accountPassword = "123456";
+accountCity = "Asansol";
+let accountState;
+
+// accountId = 2; // Not allowed
+
+accountEmail = "abc@gmail.com";
+console.log(accountId);
+console.table ([accountId,accountEmail,accountPassword,accountCity, accountState]);
+//If no value initialized it will give undefined
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"
+// Treat all JS code as newer version
+
+// alert("hello")
+// We are using NodeJs not browser
+
+console.log(3+3);
+console.log("Sahil");
+
+// Code readability should be high for better understanding
+
+let name = "Sahil"
+let age = 21
+let isLoggedIn = false
+
+// Types of data types
+/*
+    1. number
+    2. string => ""
+    3. bigint
+    4. boolean => True/false
+    5. null => standalone value
+    6. undefined
+    7. symbol
+
+    8. object
+*/
+console.log(typeof "Sahil")
+console.log(typeof null)
+// An error in JS which gives the type of null as object
